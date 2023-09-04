0e7a3dca7dfa3d23dd21b82627357c7c82b0ebb1
mayaj0yce <m.j.thayne@gmail.com>
2023-09-04 01:06:09 -0600

    data added

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,28 @@
 DROP DATABASE IF EXISTS tracker_db;
+
 CREATE DATABASE tracker_db;
+
 USE tracker_db;
 
-CREATE TABLE 
-department ( 
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-  department_name VARCHAR(30) NOT NULL
-  );
+CREATE TABLE
+    department (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        department_name VARCHAR(30) NOT NULL
+    );
 
 CREATE TABLE
     roles (
-         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(30) NOT NULL,
         salary DECIMAL NOT NULL,
-        department_id INT NOT NULL
-        Foreign Key (department_id) REFERENCES (department(id))
+        department_id INT NOT NULL Foreign Key (department_id) REFERENCES (department(id))
     );
 
 CREATE TABLE
     employee (
-      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         first_name VARCHAR(30) NOT NULL,
         last_name DECIMAL NOT NULL,
         role_id INT NOT NULL,
-        manager_id INT
-        Foreign Key (roles_id) REFERENCES (roles(id))
+        manager_id INT Foreign Key (roles_id) REFERENCES (roles(id))
     );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,32 @@
-  INSERT INTO department (name)
-  VALUES
+INSERT INTO
+    department (department_name)
+VALUES ('Meat'), ('Fruit'), ('Dairy'), ('Bakery'), ('Grocer')
 
 
-  INSERT INTO employee (first_name, last_name,role_id,manager_id)
-VALUES
- ('Ronald', 'Firbank', 1,1),
+INSERT INTO
+    roles (
+        title, 
+        salary, 
+        department_id
+    ) 
+    VALUES
+    ('Meat_Packer', 26.69, 1), 
+    ('Price_Tagger', 22.23, 2),
+    ('Shelf_stocker', 17.20, 3)
+    ('Baker', 19.28, 4),
+    ('Bag_Boy', 12.17, 5), 
+
+INSERT INTO
+    employee (
+        first_name,
+        last_name,
+        role_id,
+        manager_id
+    )
+VALUES 
+('Ronald', 'Firbank', 1, 1), 
+('Max', 'Mods', 3, 1), 
+('Sherman', 'Saus', 4, NULL), 
+('Sarah', 'Winder', 2, 3), 
+('Rob', 'Kebab', 1, 3), 
+('Kaddy', 'Brair', 2, NULL),
